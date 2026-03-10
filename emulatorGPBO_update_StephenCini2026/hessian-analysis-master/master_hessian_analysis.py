635692b6c8d714a0b876eb81563998f386e6a7f6
Stephen Cini <114932899+sscini@users.noreply.github.com>
2026-03-09 23:08:25 -0400

    Changed path to reflect folder name change

--- emulatorGPBO_update_StephenCini2026/hessian-analysis-master/master_hessian_analysis.py
+++ emulatorGPBO_update_StephenCini2026/hessian-analysis-master/master_hessian_analysis.py
@@ -27,7 +27,7 @@ Institution: University of Notre Dame
 import sys
 
 sys.path.append(
-    "/Users/scini/Documents/GitHub/ReverseEngineeringMorphogenesis/multiobjective_update_StephenCini2026"
+    "/Users/scini/Documents/GitHub/ReverseEngineeringMorphogenesis/emulatorGPBO_update_StephenCini2026"
 )
 
 # Importing libraries
